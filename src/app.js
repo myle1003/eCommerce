f8a91ec1a8122b64d5bf6c5209c287e372b189b0
MyTLTT <myle10032001@gmail.com>
2024-05-31 22:26:21 +0700

    setup env, config, connect db

---
+++ src/app.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const compression = require('compression');
+const express = require('express');
+const { default: helmet } = require('helmet');
+const morgan = require('morgan');
+const app = express();
+
+// init middlewares
+app.use(morgan("dev"))
+app.use(helmet())
+app.use(compression())
+// init db
+
+require('./dbs/init.mongodb')
+const {checkOverload} = require('./helpers/check.connect')
+checkOverload()
+// init routes
+app.get('/', (req, res,next) => {
+    return res.status(200).json({
+        message: "hihi",
+        metadata: strCompress.repeat(1000000)
+    })
+})
+//handling error
+
+
+module.exports = app
