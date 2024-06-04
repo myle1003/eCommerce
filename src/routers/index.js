5ed5b80d3e0521a45123954d10628c3bcc502f32
MyTLTT <myle10032001@gmail.com>
2024-06-04 23:49:29 +0700

    middware for singup

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,14 +1,18 @@
 'use strict'
 
 const express = require('express')
+const { apiKey, permission } = require('../auth/checkAuth')
 const router = express.Router()
 
-router.use('/v1/api', require('./access'))
+//check apiKey
 
-router.get('/', (req, res,next) => {
-    return res.status(200).json({
-        message: "hihi Tra My",
-    })
-})
+router.use(apiKey)
+
+//check permisson
+
+router.use(permission('0000'))
+
+
+router.use('/v1/api', require('./access'))
 
 module.exports = router
