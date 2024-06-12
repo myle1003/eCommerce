9c8402af9d9abec567e1dffe3a1a891b2a702c6c
MyTLTT <myle10032001@gmail.com>
2024-06-12 21:45:14 +0700

    success response

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,11 +1,18 @@
 'use strict'
 
 const AccessService = require("../services/access.service");
-
+const { OK, CREATED } = require("../core/success.response");
 class AccessController {
 
     signUp = async (req, res, next) => {
-        return res.status(201).json(await AccessService.signUp(req.body))
+        // return res.status(201).json(await AccessService.signUp(req.body))
+        new CREATED({
+            message: 'Resiserted OK',
+            metadata: await AccessService.signUp(req.body),
+            options: {
+                limit: 10,
+            }
+        }).send(res)
     }
 }
 
